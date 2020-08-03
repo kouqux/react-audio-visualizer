96c3aedc8eded17aabb6fe2cc05cbeb5be3e27bf
github Kousuke_guitar <k223.guitar@gmail.com>
2020-08-03 17:59:30 +0900

    Updates

---
+++ precache-manifest.dee5f211e73c76960cb8969a4e638fbe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f06c893d38a368409ee238e500d2f23c",
+    "url": "/react-audio-visualizer/index.html"
+  },
+  {
+    "revision": "988f0d78d121adccaefa",
+    "url": "/react-audio-visualizer/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "703c009b1c3825d25a8f",
+    "url": "/react-audio-visualizer/static/js/2.7b3086c2.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/react-audio-visualizer/static/js/2.7b3086c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "988f0d78d121adccaefa",
+    "url": "/react-audio-visualizer/static/js/main.0f31f05f.chunk.js"
+  },
+  {
+    "revision": "ebae86b7935426fd952f",
+    "url": "/react-audio-visualizer/static/js/runtime-main.faf993c1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.09e76c4b8ee2fa7774d7885600d89591.js"
+  "/react-audio-visualizer/precache-manifest.dee5f211e73c76960cb8969a4e638fbe.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-audio-visualizer/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
